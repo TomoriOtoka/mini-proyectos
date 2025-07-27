65601cc4c42f0bf229853d854c3315aad2fbf227
Valentino <tomoriotoka@gmail.com>
2025-07-27 13:25:10 -0500

    Tercer Commit
    EncontrarDuplicados (Encuentra los duplicados de una lista de 5 enteros)
    
    ObtenerMayorYMenor (Obtiene los valores mayores y menores de una lista de 5 enteros y los ingresa en un array)

---
+++ EncontrarDuplicados/EncontrarDuplicados.cs
@@ -0,0 +1,64 @@
+﻿namespace EncontrarDuplicados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Encuentra el numero de duplicados dentro de un array de 5 enteros
+            int[] enteros = new int[5];
+
+                string numeroIngresadoStr = "";
+                int numeroIngresado;
+                bool NumeroValido = true;
+
+                Console.WriteLine("Ingrese 5 enteros");
+
+                for (int i = 0; i < 5; i++)
+                {
+                    NumeroValido = true;
+                    while(NumeroValido)
+                    {
+                        numeroIngresadoStr = Console.ReadLine();
+
+                        NumeroValido = VerificarNumero(numeroIngresadoStr);
+                    }
+                
+                    numeroIngresado = Convert.ToInt32(numeroIngresadoStr);
+                    enteros[i] = numeroIngresado;
+                }
+           
+            HashSet<int> numerosDuplicados = new HashSet<int>();
+
+            int numeroDeDuplicados = 0;
+
+            foreach (int i in enteros)
+            {
+
+                if (numerosDuplicados.Contains(i))
+                {
+                    numeroDeDuplicados++;
+                }
+                else
+                {
+                    numerosDuplicados.Add(i);
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Numero de duplicados: " + numeroDeDuplicados);
+            
+        }
+        private static bool VerificarNumero(string numero1)
+        {
+            if (!int.TryParse(numero1, out int resultado))
+            {
+                Console.WriteLine("No valido");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ObtenerMayorYMenor/ObtenerMayorYMenor.cs
@@ -0,0 +1,60 @@
+﻿namespace ObtenerMayorYMenor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Obtiene los valores mayores y menores de una array de 5 enteros
+            int[] enteros = new int[5];
+
+            string numeroIngresadoStr = "";
+            int numeroIngresado;
+            bool NumeroValido = true;
+
+            Console.WriteLine("Ingrese 5 enteros");
+
+            for (int i = 0; i < 5; i++)
+            {
+                NumeroValido = true;
+                while (NumeroValido)
+                {
+                    numeroIngresadoStr = Console.ReadLine();
+
+                    NumeroValido = VerificarNumero(numeroIngresadoStr);
+                }
+
+                numeroIngresado = Convert.ToInt32(numeroIngresadoStr);
+                enteros[i] = numeroIngresado;
+            }
+
+            int[] resultado = new int[2] { enteros[0], enteros[0] };
+
+            for (int i = 0; i < enteros.Length; i++)
+            {
+                if (enteros[i] > resultado[0])
+                {
+                    resultado[0] = enteros[i];
+                }
+                else if (enteros[i] < resultado[1])
+                {
+                    resultado[1] = enteros[i];
+                }
+            }
+
+            Console.WriteLine("Numero mayor: " + resultado[0]);
+            Console.WriteLine("Numero menor: " + resultado[1]);
+        }
+        private static bool VerificarNumero(string numero1)
+        {
+            if (!int.TryParse(numero1, out int resultado))
+            {
+                Console.WriteLine("No valido");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
