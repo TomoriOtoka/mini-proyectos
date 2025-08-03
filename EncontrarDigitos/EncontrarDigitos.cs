9fd1d9253e74f13c4caba3a14907bf674c7be44c
Valentino <tomoriotoka@gmail.com>
2025-08-03 10:49:06 -0500

    Cuarto Commit
    Programa para encontrar el numero de digitos en una cadena de texto.

---
+++ EncontrarDigitos/EncontrarDigitos.cs
@@ -0,0 +1,33 @@
+﻿namespace EncontrarDigitos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            string textoIngresado;
+            textoIngresado = Console.ReadLine();
+
+            int resultado;
+            resultado = ContarDigitos(textoIngresado);
+
+            Console.WriteLine(resultado);
+
+            
+        }
+
+        public static int ContarDigitos(string texto)
+        {
+            int numeroDeDigitos = 0;
+            foreach (char i in texto)
+            {
+                if (char.IsDigit(i))
+                {
+                    numeroDeDigitos++;
+                }
+            }
+
+            return numeroDeDigitos;
+        }
+    }
+}
