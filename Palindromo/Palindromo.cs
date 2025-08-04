bfc42a873bdceba5aca4b2e32f27d18e04495837
Valentino <tomoriotoka@gmail.com>
2025-08-04 16:55:18 -0500

    Cuarto commit
    Programa para determinar si una cadena de texto es palindromo.

---
+++ Palindromo/Palindromo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Palindromo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            string texto = "";
+            texto = Console.ReadLine();
+
+            texto = texto.ToLower();
+            
+            string textoRevertido = "";
+
+            for (int i = texto.Length - 1; i >= 0; i--)
+            {
+                textoRevertido += texto[i];
+            }
+
+            if (String.Equals(texto, textoRevertido))
+            {
+                Console.WriteLine("Es palindromo");
+            }
+            else
+            {
+                Console.WriteLine("No es palindromo");
+            }
+        }
+    }
+}
