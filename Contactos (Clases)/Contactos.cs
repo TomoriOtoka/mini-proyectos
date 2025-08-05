99267b6e68b35ba5858165ece87b51b292e8eae2
Valentino <tomoriotoka@gmail.com>
2025-08-05 13:36:56 -0500

    Quinto commit
    Primer proyecto usando clases - Lista de contactos (Basica de clases)
    
    Proyecto usando manipulacion y verificacion de cadenas de texto - ContraseñaFuerte

---
+++ Contactos (Clases)/Contactos.cs
@@ -0,0 +1,36 @@
+﻿namespace Contactos__Clases_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crea contactos a partir de clases
+            Contacto Contacto1 = new Contacto("Bridget", "0964100835", "kuroide@gmail.com");
+            Contacto Contacto2 = new Contacto("Valentino", "0987159870", "tomori@gmail.com");
+
+            Contacto1.MostrarInfo();
+            Contacto2.MostrarInfo();
+        }
+
+        class Contacto
+        {
+            public string Nombre;
+            public string Telefono;
+            public string Correo;
+
+            public Contacto(string nombre, string telefono, string correo)
+            {
+                Nombre = nombre;
+                Telefono = telefono;
+                Correo = correo;
+            }
+
+            public void MostrarInfo()
+            {
+                Console.WriteLine(Nombre);
+                Console.WriteLine(Telefono);
+                Console.WriteLine(Correo);
+            }
+        }
+    }
+}
---
+++ ContrasenaFuerte/ContrasenaFuerte.cs
@@ -0,0 +1,62 @@
+﻿namespace ContrasenaFuerte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Determina si una contraseña es fuerte 
+
+            string contrasenaIngresada;
+            bool tieneNumero = false;
+            bool tieneLetraMayus = false;
+            bool tieneLetraMinus = false;
+
+            Console.WriteLine("Ingrese una contraseña");
+            Console.WriteLine("Debe tener 8 caracteres, un numero y letras mayusculas y minusculas.");
+            contrasenaIngresada = Console.ReadLine();
+
+            int longitud = 0;
+            foreach (int i in contrasenaIngresada)
+            {
+                longitud++;
+            }
+
+            for (int i = 0; i < longitud; i++)
+            {
+                if (char.IsDigit(contrasenaIngresada[i]))
+                {
+                    tieneNumero = true;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < longitud; i++)
+            {
+                if (char.IsUpper(contrasenaIngresada[i]))
+                {
+                    tieneLetraMayus = true;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < longitud; i++)
+            {
+                if (char.IsLower(contrasenaIngresada[i]))
+                {
+                    tieneLetraMinus = true;
+                    break;
+                }
+            }
+
+            int longitudMinima = 8;
+            if (longitud > longitudMinima && tieneLetraMayus && tieneLetraMinus && tieneNumero)
+            {
+                Console.WriteLine("Es segura");
+            }
+            else
+            {
+                Console.WriteLine("No es segura");
+            }
+        }
+    }
+}
