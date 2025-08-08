070cccbb4c4a8bc34e5faa90fc85f780669e711d
Valentino <tomoriotoka@gmail.com>
2025-08-08 12:54:45 -0500

    Septimo Commit
    Calculadora de promedio, maximo y minimo de n numeros
    
    Verificador de texto (mayusculas y puntuacion)

---
+++ CalculadoraEstadisticas/CalculadoraEstadisticas.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CalculadoraEstadisticas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lee n variables del usuario, muestra el valor maximo, minimo y promedio
+            int n = 0;
+            int numeroMaximo = 0;
+            int numeroMinimo = 0;
+            double promedio = 0;
+            int numeroIngresado = 0;
+            double suma = 0;
+
+            Console.WriteLine("Ingrese la cantidad de numeros");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("Ingrese los numeros");
+            numeroIngresado = Convert.ToInt32(Console.ReadLine());
+
+            numeroMaximo = numeroIngresado;
+            numeroMinimo = numeroIngresado;
+            suma += numeroIngresado;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                numeroIngresado = Convert.ToInt32((Console.ReadLine()));
+
+                if (numeroIngresado > numeroMaximo)
+                {
+                    numeroMaximo = numeroIngresado;
+                }
+                if (numeroIngresado < numeroMinimo)
+                {
+                    numeroMinimo = numeroIngresado;
+                }
+
+                suma += numeroIngresado;
+            }
+
+            promedio = suma / n;
+
+            Console.Clear();
+            Console.WriteLine("Promedio: " + promedio);
+            Console.WriteLine("Numero Maximo: " + numeroMaximo);
+            Console.WriteLine("Numero Minimo: " + numeroMinimo);
+        }
+    }
+}
---
+++ VerificadorTexto/VerificadorTexto.cs
@@ -0,0 +1,39 @@
+﻿namespace VerificadorTexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Verifica si empieza con mayuscula y termina con puntuacion
+
+            string textoIngresado;
+            bool empiezaEnMayuscula = false;
+            bool terminaEnPunto = false;
+            // Ingreso de texto (string)
+            Console.WriteLine("El texto ingresado debe empezar en mayusculas y terminar en puntuacion");
+            textoIngresado = Console.ReadLine();
+
+            // Verficar si empieza con mayuscula
+            if (char.IsUpper(textoIngresado[0]))
+            {
+                empiezaEnMayuscula = true;
+            }
+
+            // Verificar si termina con un punto
+            if (char.IsPunctuation(textoIngresado[textoIngresado.Length - 1]))
+            {
+                terminaEnPunto = true;
+            }
+
+            // Imprimir resultado
+            if (empiezaEnMayuscula && terminaEnPunto)
+            {
+                Console.WriteLine("Aprobado");
+            }
+            else
+            {
+                Console.WriteLine("No aprobado");
+            }
+        }
+    }
+}
